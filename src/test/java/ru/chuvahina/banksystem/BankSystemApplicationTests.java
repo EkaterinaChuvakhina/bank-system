4f76e538d58687f957979e0218fa61420783dbcb
Ekaterina Chuvahina <chuvahina20@gmail.com>
2021-04-08 15:41:49 +0700

    Initial commit

---
+++ src/test/java/ru/chuvahina/banksystem/BankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chuvahina.banksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
