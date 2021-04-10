0b50dd096a7ffdcdd2201dcd8c142e830b465248
Ekaterina Chuvahina <chuvahina20@gmail.com>
2021-04-10 18:16:42 +0700

    add fields to BillResponseDTO and lombok annotations

--- src/main/java/ru/chuvahina/banksystem/dto/BillResponseDTO.java
+++ src/main/java/ru/chuvahina/banksystem/dto/BillResponseDTO.java
@@ -1,2 +1,14 @@
-package ru.chuvahina.banksystem.dto;public class BillDTO {
+package ru.chuvahina.banksystem.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+public class BillResponseDTO {
+    private Long billId;
+    private BigDecimal amount;
+    private Boolean isDefault;
 }
