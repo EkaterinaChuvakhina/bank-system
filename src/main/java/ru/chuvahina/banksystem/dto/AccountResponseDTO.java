47c34cd130f0cfbb619ade787cbba96c5229c68a
Ekaterina Chuvahina <chuvahina20@gmail.com>
2021-04-10 18:14:04 +0700

    add fields to ResponseDTO

--- src/main/java/ru/chuvahina/banksystem/dto/AccountResponseDTO.java
+++ src/main/java/ru/chuvahina/banksystem/dto/AccountResponseDTO.java
@@ -1,2 +1,17 @@
-package ru.chuvahina.banksystem.dto;public class AccountResponseDTO {
+package ru.chuvahina.banksystem.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class AccountResponseDTO {
+    private Long accountId;
+    private String name;
+    private String email;
+    private List<BillResponseDTO> bills;
 }
