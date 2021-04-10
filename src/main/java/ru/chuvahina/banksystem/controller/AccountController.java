9f86c0a4af33b03fbf2a47b30fe984d1e45208c2
Ekaterina Chuvahina <chuvahina20@gmail.com>
2021-04-10 18:12:59 +0700

    add methods and fields to Account

--- src/main/java/ru/chuvahina/banksystem/controller/AccountController.java
+++ src/main/java/ru/chuvahina/banksystem/controller/AccountController.java
@@ -1,2 +1,30 @@
-package ru.chuvahina.banksystem.controller;public class AccountController {
+package ru.chuvahina.banksystem.controller;
+
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.chuvahina.banksystem.dto.AccountRequestDTO;
+import ru.chuvahina.banksystem.dto.AccountResponseDTO;
+import ru.chuvahina.banksystem.mapper.AccountMapper;
+import ru.chuvahina.banksystem.service.AccountService;
+
+@RestController
+public class AccountController {
+    private final AccountMapper accountMapper = Mappers.getMapper(AccountMapper.class);
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/accounts/{accountId}")
+    public AccountResponseDTO getById(@PathVariable Long accountId) {
+        return accountMapper.toResponseDTO(accountService.findById(accountId));
+    }
+
+    @PostMapping("/accounts")
+    public Long create(@RequestBody AccountRequestDTO accountRequestDTO) {
+        return accountService.save(accountMapper.toAccount(accountRequestDTO));
+    }
 }
