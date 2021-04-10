904b102e577b305ce3fadfb3b989a9dea5bf7aad
Ekaterina Chuvahina <chuvahina20@gmail.com>
2021-04-10 18:15:30 +0700

    add fields and methods to Bill

--- src/main/java/ru/chuvahina/banksystem/entity/Bill.java
+++ src/main/java/ru/chuvahina/banksystem/entity/Bill.java
@@ -1,2 +1,59 @@
-package ru.chuvahina.banksystem.entity;public class Bill {
+package ru.chuvahina.banksystem.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long billId;
+
+    @JsonProperty("amount")
+    private BigDecimal amount;
+
+    @JsonProperty("isDefault")
+    private Boolean isDefault;
+
+    public Bill() {
+    }
+
+    public Bill(BigDecimal amount, Boolean isDefault) {
+        this.amount = amount;
+        this.isDefault = isDefault;
+    }
+
+    public Long getBillId() {
+        return billId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Boolean getIsDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(Boolean ifDefault) {
+        this.isDefault = ifDefault;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "billId=" + billId +
+                ", amount=" + amount +
+                ", ifDefault=" + isDefault +
+                '}';
+    }
 }
