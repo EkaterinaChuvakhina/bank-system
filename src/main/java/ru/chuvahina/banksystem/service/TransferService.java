2a389b622aa74d9836b18290433af8fbe683e00e
Ekaterina Chuvahina <chuvahina20@gmail.com>
2021-04-11 15:03:20 +0700

    transferring a findDefaultBill method to a separate class

--- src/main/java/ru/chuvahina/banksystem/service/TransferService.java
+++ src/main/java/ru/chuvahina/banksystem/service/TransferService.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.chuvahina.banksystem.entity.Account;
 import ru.chuvahina.banksystem.entity.Bill;
-import ru.chuvahina.banksystem.exception.NowDefaultBillException;
+import ru.chuvahina.banksystem.utill.AccountUtils;
 
 import java.math.BigDecimal;
 
@@ -16,8 +16,8 @@ public class TransferService {
     public Object transfer(Long accountIdFrom, Long accountIdTo, BigDecimal amount) {
         Account accountFrom = accountService.findById(accountIdFrom);
         Account accountTo = accountService.findById(accountIdTo);
-        Bill billFrom = findDefaultBill(accountFrom);
-        Bill billTo = findDefaultBill(accountTo);
+        Bill billFrom = AccountUtils.findDefaultBill(accountFrom);
+        Bill billTo = AccountUtils.findDefaultBill(accountTo);
         billFrom.setAmount(billFrom.getAmount().subtract(amount));
         billFrom.setAmount(billTo.getAmount().add(amount));
         accountService.update(accountFrom);
@@ -25,6 +25,5 @@ public class TransferService {
         return "OK";
 
     }
-    
-    }
 }
+
